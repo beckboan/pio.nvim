af991fbc3ad0f8c473192c8d79d95927cf5d4da6
Beck Boan <beckmboan@gmail.com>
2024-05-21 23:49:22 -0400

    Added async previewer for package names

--- lua/pio/package.lua
+++ lua/pio/package.lua
@@ -1,5 +1,6 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
+local previewers = require("telescope.previewers")
 local conf = require("telescope.config").values
 
 local actions = require("telescope.actions")
@@ -50,6 +51,57 @@ local live_lib_search = function(opts, name)
 				results = entries,
 			}),
 			sorter = conf.generic_sorter(opts),
+			previewer = previewers.new_buffer_previewer({
+				title = "Library Preview",
+				define_preview = function(self, entry)
+					local package_name = entry.value
+					local command = { "pio", "pkg", "show", package_name } -- Command to get package details
+
+					--TODO: ADD LOG STATEMENTS FOR COMMAND RUN
+
+					local function on_output(job_id, data, event)
+						if event == "stdout" and #data > 0 then
+							--Removing empty lines from data
+
+							local non_empty_lines = {}
+							for _, line in ipairs(data) do
+								if line ~= "" then
+									table.insert(non_empty_lines, line)
+								end
+							end
+
+							if vim.api.nvim_buf_is_valid(self.state.bufnr) then
+								vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, non_empty_lines)
+								require("telescope.previewers.utils").highlighter(self.state.bufnr, "plaintext")
+							end
+						elseif event == "stderr" and #data > 0 then
+							--TODO: ADD LOG STATEMENTS FOR STD ERR
+						end
+					end
+
+					local job_id = vim.fn.jobstart(command, {
+						stdout_buffered = true,
+						on_stdout = on_output,
+						on_stderr = on_output,
+						on_exit = function(job_id, exit_code, event)
+							if exit_code ~= 0 then
+								if vim.api.nvim_buf_is_valid(self.state.bufnr) then
+									vim.api.nvim_buf_set_lines(
+										self.state.bufnr,
+										0,
+										-1,
+										false,
+										{ "Error: failed to retrieve package details" }
+										--TODO: ADD LOG STATEMENTS FOR DETAIL FAILURE
+									)
+								end
+							end
+						end,
+					})
+
+					--TODO: ADD LOG STATEMENTS FOR JOB
+				end,
+			}),
 		})
 		:find()
 end
