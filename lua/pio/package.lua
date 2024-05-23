11bb990d4228e107b89c009120fa8675b94c37ca
Beck Boan <beckmboan@gmail.com>
2024-05-22 21:37:23 -0400

    Coroutines running to gather package quicker - need to add preservation of sorting

--- lua/pio/package.lua
+++ lua/pio/package.lua
@@ -49,11 +49,11 @@ local live_lib_search = function(opts, name)
 	end
 
 	-- Picker setup
-	local entries = packages
+	local entries = {}
 	local picker = pickers.new(opts, {
 		prompt_title = "PIO Command",
 		finder = finders.new_table({
-			results = entries,
+			results = { "Loading..." },
 		}),
 		sorter = conf.generic_sorter(opts),
 		previewer = previewers.new_buffer_previewer({
@@ -64,20 +64,26 @@ local live_lib_search = function(opts, name)
 
 				-- Run the command asynchronously
 				local function on_output(job_id, data, event)
-					if event == "stdout" and #data > 0 then
-						local non_empty_lines = {}
-						for _, line in ipairs(data) do
-							if line ~= "" then
-								table.insert(non_empty_lines, line)
-							end
-						end
-
+					if package_name == "Loading..." then
 						if vim.api.nvim_buf_is_valid(self.state.bufnr) then
-							vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, non_empty_lines)
-							require("telescope.previewers.utils").highlighter(self.state.bufnr, "plaintext")
+							vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { "Loading..." })
 						end
-					elseif event == "stderr" and #data > 0 then
-						--TODO: Log error
+					else
+						if event == "stdout" and #data > 0 then
+							local non_empty_lines = {}
+							for _, line in ipairs(data) do
+								if line ~= "" then
+									table.insert(non_empty_lines, line)
+								end
+							end
+
+							if vim.api.nvim_buf_is_valid(self.state.bufnr) then
+								vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, non_empty_lines)
+								require("telescope.previewers.utils").highlighter(self.state.bufnr, "plaintext")
+							end
+						elseif event == "stderr" and #data > 0 then
+							--TODO: Log error
+						end
 					end
 				end
 
@@ -105,69 +111,71 @@ local live_lib_search = function(opts, name)
 		}),
 	})
 
-	-- Function to fetch remaining pages asynchronously
-	local function fetch_remaining_pages()
-		local fetched_pages = 1
+	local function fetch_page(page)
+		local command = "pio pkg search 'type:library " .. name .. "' -s popularity -p " .. page
+		vim.fn.jobstart(command, {
+			stdout_buffered = true,
+			on_stdout = function(_, data, _)
+				local parsed_data = commands.parse_command(data)
 
-		local function fetch_page(page)
-			local command = "pio pkg search 'type:library " .. name .. "' -s popularity -p " .. page
-			vim.fn.jobstart(command, {
-				stdout_buffered = true,
-				on_stdout = function(_, data, _)
-					local parsed_data = commands.parse_command(data)
-
-					if #parsed_data > 0 then
-						local new_packages = {}
-						for i = 2, #parsed_data do -- Start from the second entry
-							if parsed_data[i][1] ~= "" or nil then
-								table.insert(new_packages, parsed_data[i][1])
-							end
+				if #parsed_data > 0 then
+					local new_packages = {}
+					for i = 2, #parsed_data do -- Start from the second entry
+						if parsed_data[i][1] ~= "" or nil then
+							table.insert(new_packages, parsed_data[i][1])
 						end
-						--TODO: Log package  results
-						for _, pkg in ipairs(new_packages) do
-							if not vim.tbl_contains(entries, pkg) then
-								table.insert(entries, pkg)
-							end
+					end
+					--TODO: Log package  results
+					for _, pkg in ipairs(new_packages) do
+						if not vim.tbl_contains(entries, pkg) then
+							table.insert(entries, pkg)
 						end
+					end
 
-						picker:refresh(
-							finders.new_table({
-								results = entries,
-							}),
-							{
-								reset_prompt = false,
-							}
-						)
-					end
-				end,
-				on_stderr = function(job_id, data, event)
-					if #data > 0 then
-						-- TODO: Log error
-					end
-				end,
-				on_exit = function(job_id, exit_code, event)
-					fetched_pages = fetched_pages + 1
-					if fetched_pages <= total_pages then
-						fetch_page(fetched_pages)
-					end
-				end,
-			})
-		end
-
-		fetch_page(fetched_pages + 1)
+					picker:refresh(
+						finders.new_table({
+							results = entries,
+						}),
+						{
+							reset_prompt = false,
+						}
+					)
+				end
+			end,
+			on_stderr = function(job_id, data, event)
+				if #data > 0 then
+					-- TODO: Log error
+				end
+			end,
+		})
 	end
-
-	-- Start the picker
+	local function fetch_remaining_pages()
+		local max_coroutines = 10
+		local active_coroutines = 0
+		local page = 1
+		while page <= total_pages do
+			while active_coroutines >= max_coroutines do
+				coroutine.yield()
+			end
+			coroutine.wrap(function()
+				active_coroutines = active_coroutines + 1
+				fetch_page(page)
+				active_coroutines = active_coroutines - 1
+			end)()
+			page = page + 1
+		end
+	end
 	picker:find()
 	-- Fetch remaining pages
 	fetch_remaining_pages()
 end
+-- Function to fetch remaining pages asynchronously
 
 local test_lib = function()
 	local results = { "test1", "test2", "test3", "test4" }
 	live_lib_search({}, results)
 end
 
-live_lib_search(require("telescope.themes").get_dropdown({}), "division")
+live_lib_search(require("telescope.themes").get_dropdown({}), "json")
 
 -- test_lib()
